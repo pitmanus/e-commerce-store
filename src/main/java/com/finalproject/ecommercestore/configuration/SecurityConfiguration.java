63b19d67025744fbbc2a7366023f8aeeaa62959b
Petro Manus <petrmanus@gmail.com>
2020-12-06 14:59:42 +0100

    Now user can see account information on user-account.html page.

--- src/main/java/com/finalproject/ecommercestore/configuration/SecurityConfiguration.java
+++ src/main/java/com/finalproject/ecommercestore/configuration/SecurityConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -76,4 +78,6 @@ auth.inMemoryAuthentication()
         .password(passwordEncoder().encode("test"))
         .roles("ADMIN");
     }
+
+
 }
--- src/main/java/com/finalproject/ecommercestore/controller/UserPageController.java
+++ src/main/java/com/finalproject/ecommercestore/controller/UserPageController.java
@@ -1,14 +1,32 @@
 package com.finalproject.ecommercestore.controller;
 
+import com.finalproject.ecommercestore.configuration.SecurityConfiguration;
+import com.finalproject.ecommercestore.model.dto.UserDto;
+import com.finalproject.ecommercestore.service.UserService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class UserPageController {
 
+    private UserService userService;
+
+
+    public UserPageController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GetMapping("/user-account")
-    public String showUserAccountPage(){
+    public String showUserAccountPage(Model model){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        String userName = auth.getName();
+        UserDto userDto = userService.getUserByUserName(userName);
+        model.addAttribute("user", userDto);
         return "user-account";
     }
+
 }
